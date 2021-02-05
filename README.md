c1f5f2180ebe73a4d4182c8dea485323c92f2fe8
Emma Jonas <enjonas@hotmail.com>
2021-02-04 20:32:09 -0600

    updated formatting and README

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Shell Project
 
 Designed and implemented a simple shell in C for my Operating Systems class.
+
+## Supports
+
+- all basic shell commands
+- `cd` command to change directory
+- pipes
+- input and output redirection
+- background processes by adding `&` to the end of any command (still buggy with interactive processes, like `top &`)
+- `!!` history feature to execute the previous command
+- exit command
+
+### Note
+
+- no support for quoting arguments or escaping whitespace
+- no support for the chaining of pipes and/or redirection operators
+- commands must be in one line
+- max input length is 80 characters
+- arguments must be separated by whitespace (including `&`)
--- project1.c
+++ project1.c
@@ -10,32 +10,40 @@
 #define MAX_LINE 80 /* The maximum length command */
 #define DELIM " \t\r\n\a"
 
-/* if `is_out = 1`, redirects the output from the command args1 to filname args2
-  else, redirects the input from filename args2 into command args1 */
+int is_background(char **args) {
+  int background = 0;
+  size_t count = 0;
+  while (args[count] != NULL) {
+    ++count;
+  }
+  /* check if the command should run in the background */
+  if (strcmp(args[count - 1], "&") == 0) {
+    background = 1;
+    args[count - 1] = NULL;
+  }
+  return background;
+}
+
+/* if `is_out = 1`, redirects the output from the command args1 to filename args2
+  else, redirects the input from filename args2 into command args1,
+  supports background processes for command args1 if followed by `&` */
 int run_red(char **args1, char **args2, int is_out) {
   pid_t pid, ppid;
-  int status;
-  int out, in, background;
-
-  size_t count = 0;
-    while (args1[count] != NULL) {
-        ++count;
-    }
+  int status, out, in, background;
 
   pid = fork();
 
-  if(strcmp(args1[count - 1], "&") == 0) {
-        background = 1;
-        args1[count - 1] = NULL;
-        if (pid > 0) {
-        printf("PID: %d\n", pid);
-        }
-    }
+  background = is_background(args1);
 
-  if(pid < 0) {
+  if (background && pid > 0) {
+    /* print the pid to the console */
+    printf("PID: %d\n", pid);
+  }
+
+  if (pid < 0) {
     fprintf(stderr, "run_red: fork failed\n");
     return 1;
-  } else if(pid == 0) {
+  } else if (pid == 0) {
     if (is_out) {
       out = open(args2[0], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
       dup2(out, 1);
@@ -48,10 +56,10 @@ int run_red(char **args1, char **args2, int is_out) {
     if (in == -1 && args2[0] != NULL) {
       fprintf(stderr, "mysh: %s: no such file or directory\n", args2[0]);
       exit(EXIT_FAILURE);
-    } else if(in == -1) {
+    } else if (in == -1) {
       fprintf(stderr, "mysh: please provide an input source\n");
       exit(EXIT_FAILURE);
-    } else if(out == -1) {
+    } else if (out == -1) {
       fprintf(stderr, "mysh: please provide an output source\n");
       exit(EXIT_FAILURE);
     } else if (execvp(args1[0], args1) == -1) {
@@ -70,14 +78,14 @@ int launch_pipe(char **args1, char **args2) {
   pid_t pid;
   int status;
   
-  if(pipe(fd) != 0) {
+  if (pipe(fd) != 0) {
     fprintf(stderr, "launch_pipe: failed to create pipe.\n");
     return 1;
   }
 
   pid = fork();
 
-  if(pid < 0) {
+  if (pid < 0) {
     fprintf(stderr, "launch_pipe: fork failed\n");
     return 1;
   } else if(pid == 0) {
@@ -98,19 +106,27 @@ int launch_pipe(char **args1, char **args2) {
   return 1;
 }
 
-/* creates a parent process for launch_pipe */
+/* creates a parent process for launch_pipe, supports background
+  processes if second command followed by `&` */
 int run_pipe(char **args1, char **args2) {
   pid_t pid, ppid;
-  int status;
+  int status, background = 0;
 
   pid = fork();
 
-  if(pid < 0) {
+  background = is_background(args2);
+
+  if (background && pid > 0) {
+    /* print the pid to the console */
+    printf("PID: %d\n", pid);
+  }
+
+  if (pid < 0) {
     fprintf(stderr, "run_pipe: fork failed\n");
     return 1;
-  } else if(pid == 0) {
+  } else if (pid == 0) {
     status = launch_pipe(args1, args2);
-  } else {
+  } else if (!background) {
     ppid = waitpid(pid, &status, WUNTRACED);
   }
   return 1;
@@ -136,45 +152,38 @@ int run_exit(char **args) {
   such as ls, ps, pwd, etc., has support for the & feature
   which runs the child process in the background */
 int run_command(char **args) {
-    pid_t pid, ppid;
-    int status;
-    int background = 0;
+  pid_t pid, ppid;
+  int status, background = 0;
 
-    size_t count = 0;
-    while (args[count] != NULL) {
-        ++count;
-    }
-    /* fork a child process */
-    pid = fork();
+  /* fork a child process */
+  pid = fork();
     
-    /* check if the command should run in the background */
-    if(strcmp(args[count - 1], "&") == 0) {
-        background = 1;
-        args[count - 1] = NULL;
-        if (pid > 0) {
-        printf("PID: %d\n", pid);
-        }
-    }
+  background = is_background(args);
 
-    if (pid < 0) { /* error occurred */ 
-        fprintf(stderr, "run_command: fork failed\n");
-        return 1;
-    } else if (pid == 0) { /* child process */
-      /* runs properly with regular commands,  */
-        if (background) {
-          int out = open("/dev/null", O_WRONLY);
-          dup2(out, 1);
-          close(out);
-        }
-        if (execvp(args[0], args) == -1) {
-          fprintf(stderr, "mysh: %s: command not found\n", args[0]);
-          exit(EXIT_FAILURE);
-        }
-    } else if(!background){ /* parent process */
-    /* parent will wait for the child to complete */
-      ppid = waitpid(pid, &status, WUNTRACED);
-    }
+  if (background && pid > 0) {
+    /* print the pid to the console */
+    printf("PID: %d\n", pid);
+  }
+
+  if (pid < 0) { /* error occurred */ 
+    fprintf(stderr, "run_command: fork failed\n");
     return 1;
+  } else if (pid == 0) { /* child process */
+    /* if to run in the background, redirect output to /dev/null */
+      if (background) {
+        int out = open("/dev/null", O_WRONLY);
+        dup2(out, 1);
+        close(out);
+      }
+      if (execvp(args[0], args) == -1) {
+        fprintf(stderr, "mysh: %s: command not found\n", args[0]);
+        exit(EXIT_FAILURE);
+      }
+  } else if (!background) { /* parent process */
+  /* parent will wait for the child to complete */
+    ppid = waitpid(pid, &status, WUNTRACED);
+  }
+  return 1;
 }
 
 /* execute builtin shell commands or linux commands */
@@ -187,23 +196,21 @@ int execute(char **args) {
     return run_cd(args);
   }
 
-  if(strcmp(args[0], "exit") == 0) {
+  if (strcmp(args[0], "exit") == 0) {
     return run_exit(args);
   }
 
   return run_command(args);
 }
 
-/* splits the input line into two arrays: args before and after the
-  pipe or i/o redirection. Assumes that only one of '|', '>', or '<'
+/* splits the input into two arrays: the args before and after the pipe
+  or i/o redirection operators. Assumes that only one of '|', '>', or '<'
   will be present in the input, does not support the chaining of
   these operators */
 int split_other(char *input) {
   char **args = malloc((MAX_LINE/2 + 1) * sizeof(char*));
   char **args1 = malloc((MAX_LINE/2 + 1) * sizeof(char*));
-  int pip = 0;
-  int red_out = 0;
-  int red_in = 0;
+  int pip = 0, red_out = 0, red_in = 0;
 
     if (!args || !args1) {
       fprintf(stderr, "split_other: malloc error\n");
@@ -211,8 +218,7 @@ int split_other(char *input) {
     }
 
     char *token = strtok(input, DELIM);
-    int i = 0;
-    int j = 0;
+    int i = 0, j = 0;
 
     while(token != NULL) {
       if (strcmp(token, "|") == 0){
@@ -241,15 +247,15 @@ int split_other(char *input) {
 
     if (pip) {
       return run_pipe(args, args1);
-    } else if(red_out) {
+    } else if (red_out) {
       return run_red(args, args1, 1);
-    } else if(red_in) {
+    } else if (red_in) {
       return run_red(args, args1, 0);
     }
     return 1;
 }
 
-/* splits the input line into an array of arguments with whitespace
+/* splits the input into an array of arguments with whitespace
   removed, assumes that the input given is <= 80 characters */
 char **split_args(char *input) {
     char **args = malloc((MAX_LINE/2 + 1) * sizeof(char*));
@@ -261,7 +267,7 @@ char **split_args(char *input) {
 
     char *token = strtok(input, DELIM);
     int i = 0;
-    while(token != NULL) {
+    while (token != NULL) {
       args[i++] = token;
       token = strtok (NULL, DELIM);
     }
@@ -276,27 +282,27 @@ char *read_input(void) {
     ssize_t chars = getline(&input, &size, stdin);
 
     if (chars == -1){
-        if (feof(stdin)) {
-          exit(EXIT_SUCCESS);
-        } else  {
-          fprintf(stderr, "read_input: getline error\n");
-          exit(EXIT_FAILURE);
-        }
+      if (feof(stdin)) {
+        exit(EXIT_SUCCESS);
+      } else {
+        fprintf(stderr, "read_input: getline error\n");
+        exit(EXIT_FAILURE);
+      }
     }
     return input;
 }
 
-/* trim the leading and trailing whitespace of the input line */
+/* trim the leading and trailing whitespace of the input */
 char *trim_space(char *str) {
   char *str2;
   /* leading */
-  while(isspace((unsigned char)*str)) str++;
+  while (isspace((unsigned char)*str)) str++;
 
   if(*str == 0)
     return str;
   /* trailing */
   str2 = str + strlen(str) - 1;
-  while(str2 > str && isspace((unsigned char)*str2)) str2--;
+  while (str2 > str && isspace((unsigned char)*str2)) str2--;
 
   str2[1] = '\0';
   return str;
@@ -306,11 +312,8 @@ int main(void) {
   int should_run = 1; /* flag to determine when to exit program */
   char **args; /* command line arguments */
   char *input;
-  int status;
   char history[MAX_LINE];
-  int has_hist = 0;
-  int pip = 0;
-  int red = 0;
+  int status = 0, has_hist = 0, pip = 0, red = 0;
 
   while (should_run) {
     printf("mysh:~$ ");
@@ -318,32 +321,32 @@ int main(void) {
     strcpy(input, trim_space(input));
 
     /* check if the history command was entered */
-    if(strcmp(input, "!!") == 0){
-      /* no previous command has been entered */
+    if (strcmp(input, "!!") == 0) {
+      /* if no previous command has been entered */
       if (has_hist == 0) {
         printf("No commands in history.\n");
         continue;
       } else {
-          /* a previous command has been logged, so change the input
-            string to the command saved in history */
-          strcpy(input, history);
-          /* print the command to the console */
-          printf("%s\n", input);
+        /* a previous command has been logged, so copy the command 
+          saved in history over the input string */
+        strcpy(input, history);
+        /* print the command to the console */
+        printf("%s\n", input);
       }
-    } else if(strcmp(input, "") != 0){
+    } else if (strcmp(input, "") != 0) {
       /* if anything more than whitespace was entered, save the
         command to history */
-        has_hist = 1;
-        strcpy(history, input);
+      has_hist = 1;
+      strcpy(history, input);
     }
     
     /* check for pipes or redirection */
     int i = 0;
-    while(input[i] != '\0') {
-      if (input[i] == '|'){
+    while (input[i] != '\0') {
+      if (input[i] == '|') {
         pip = 1;
       }
-      if(input[i] == '>' || input[i] == '<') {
+      if (input[i] == '>' || input[i] == '<') {
         red = 1;
       }
       i++;
